1a8e8f4d1fa4829d8e61fff1116547a0af5e19a9
Ganzadonatien <niyocroirealine@gmail.com>
2025-02-21 22:11:02 +0200

    Complete KIMELIA Shop

--- frontnend/vite.config.js
+++ frontnend/vite.config.js
@@ -1,14 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
-	server: {
-		proxy: {
-			"/api": {
-				target: "http://localhost:5000",
-			},
-		},
-	},
+  plugins: [react()],
+  server: {
+    host: true,  // Allow external access if needed
+    port: 5173,
+  },
+  build: {
+    outDir: "dist", // Ensure output goes to "dist"
+  },
 });
